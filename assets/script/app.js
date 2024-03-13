f64bfc7ff8a29bbef71aa138bad18fbd17336a7b
Evan Kennedy <evanckennedy@protonmail.com>
2024-03-13 09:30:52 -0500

    create login dialog-Saad

--- assets/script/app.js
+++ assets/script/app.js
@@ -9,4 +9,10 @@ import * as utils from './utils.js';
 /*  Organizer                                            */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-utils.print('Welcome');
+const modal = document.querySelector('.modal');
+const openLoginModal = document.querySelector('.open-login-modal');
+const closeLoginModal = document.querySelector('.close-modal-button');
+
+openLoginModal.addEventListener('click', () => {
+  modal.showModal();
+})
